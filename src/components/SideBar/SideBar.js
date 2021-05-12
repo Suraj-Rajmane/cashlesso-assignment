53e0f6e1be846ba1d656cda9693a15c6c27a1a40
suraj009 <surajrajmane18@gmail.com>
2021-05-12 18:12:09 +0530

    added some responsiveness

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,8 +5,12 @@ const SideBar = () => {
     return (
         <div className="side-bar">
             <div className="element">Template Management</div>
-            <div className="element different">View Templates</div>
-            <div className="element different">Create Templates</div>
+            <div className="element">
+                <div className="different">View Templates</div>
+            </div>
+            <div className="element">
+                <div className="different">Create Templates</div>
+            </div>
             <div className="element">Customer Segment Management</div>
             <div className="element">Manage Campaigns</div>
         </div>
